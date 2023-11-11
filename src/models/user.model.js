cf131687d1d6fb1e1203221c96eb42f5a1bb2172
Prathyarthi <prathyarti@gmail.com>
2023-11-11 21:31:31 +0530

    Added jwt,bcrypt and video model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose"
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
 const userSchema = new Schema({
     username: {
@@ -37,12 +39,49 @@ const userSchema = new Schema({
     ],
     password: {
         type: String,
-        required:[true,"Password is required"]
+        required: [true, "Password is required"]
     },
     refreshToken: {
         type: String,
-        
+
     }
 }, { timestamps: true })
 
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+// Designing custom method to check whether password and encrypted password is correct or not?
+// This is the syntax for creating custom method
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullName: this.fullName
+    },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign({
+        _id: this._id,
+    },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 export const User = mongoose.model("User", userSchema)
